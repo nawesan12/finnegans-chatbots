cc809bfc7b4fd90b111ab3671722cb7237a9d800
Nahuel Santillan <84944031+nawesan12@users.noreply.github.com>
2025-09-04 17:05:35 -0300

    chore: fix minor inconsistencies

--- src/lib/animations.ts
+++ src/lib/animations.ts
@@ -1,10 +1,11 @@
+import { Transition } from "framer-motion";
+
 // Animation Variants
 export const pageVariants = {
   initial: { opacity: 0, y: 20 },
   in: { opacity: 1, y: 0 },
   out: { opacity: 0, y: -20 },
 };
-import { Transition } from "framer-motion";
 
 export const pageTransition: Transition = {
   type: "tween",
